20627d55aef661554a5862fa49063f425e578ab7
Bianca <bihehappy@gmail.com>
2023-01-12 12:39:08 -0300

    SomaSimples

---
+++ P1003.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.util.Scanner; 
+ 
+public class P1003 {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+ 
+        Scanner input = new Scanner(System.in);
+        //int A = 30, B = 10;    
+        //System.out.println("Digite um nummero: ");
+        int A = input.nextInt();
+        //System.out.println("Digite outro nummero: ");
+        int B = input.nextInt();
+        int SOMA = A + B;
+        System.out.println("SOMA = " + SOMA);
+
+        input.close();
+ 
+    }
+ 
+}
+
+
+/* 
+  Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. 
+  A seguir escrever o valor desta variável.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. 
+Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+*/
