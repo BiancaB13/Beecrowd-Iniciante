4244dcd177796ec5f10e1f772e1a70291322c0fd
Bianca <bihehappy@gmail.com>
2023-01-16 10:35:40 -0300

    Salário com bônus

--- P1009.java
+++ P1009.java
@@ -5,10 +5,10 @@ public class P1009 {
  
         Scanner input = new Scanner(System.in);
 
-        String nome = input.next();
+        //String nome = input.next();
         double fixo = input.nextDouble();
         double vendas = input.nextDouble();
-        
+
         double TOTAL = fixo + ((vendas * 15)/100) ;
         System.out.printf("TOTAL = R$ %.2f\n", TOTAL);
         input.close();
